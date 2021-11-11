51556040de355fddb9823c4bb0b314d10e92a6b7
yogamahhendra <yoga.mahendra2000@gmail.com>
2021-11-11 16:27:46 +0800

    index

--- index.php
+++ index.php
@@ -1,12 +1,40 @@
 <?php include 'layout/header.php'; ?>
+<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
 <title>AAA</title>
 </head>
 
-<body>
+<body class="bg-gray-200">
     <?php include 'layout/navbar.php' ?>
+    <div class="mx-auto w-9/12 bg-red-400">
+        <div class="swiper mySwiper swiper-initialized swiper-horizontal swiper-pointer-events">
+            <div class="swiper-wrapper" id="swiper-wrapper-e6d2bb55f20131018" aria-live="polite" style="transition-duration: 0ms; transform: translate3d(-3072px, 0px, 0px);">
+                <div class="swiper-slide swiper-slide-active" role="group" aria-label="1 / 9">
+                <img class="h-80 object-none object-bottom" src="public/images/tes.webp">
+            </div>
+                <div class="swiper-slide swiper-slide-next" role="group" aria-label="2 / 9">
+                <img class="object-cover"src="public/images/tes.webp">
+            </div>
+                <div class="swiper-slide" role="group" aria-label="3 / 9" >Slide 3</div>
+                <div class="swiper-slide " role="group" aria-label="4 / 9">Slide 4</div>
+                <div class="swiper-slide" role="group" aria-label="5 / 9">Slide 5</div>
+                <div class="swiper-slide" role="group" aria-label="6 / 9">Slide 6</div>
+                <div class="swiper-slide" role="group" aria-label="7 / 9">Slide 7</div>
+                <div class="swiper-slide" role="group" aria-label="8 / 9">Slide 8</div>
+                <div class="swiper-slide swiper-slide-prev" role="group" aria-label="9 / 9">Slide 9</div>
+            </div>
+            <div class="swiper-pagination swiper-pagination-bullets swiper-pagination-horizontal"><span class="swiper-pagination-bullet"></span><span class="swiper-pagination-bullet"></span><span class="swiper-pagination-bullet swiper-pagination-bullet-active"></span><span class="swiper-pagination-bullet"></span><span class="swiper-pagination-bullet"></span><span class="swiper-pagination-bullet"></span><span class="swiper-pagination-bullet"></span><span class="swiper-pagination-bullet"></span><span class="swiper-pagination-bullet"></span></div>
+            <span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span>
+        </div>
+
+    </div>
 
 
-    <?php include 'layout/footer.php'?>
-
-
-
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+    <script>
+        var swiper = new Swiper(".mySwiper", {
+            pagination: {
+                el: ".swiper-pagination",
+            },
+        });
+    </script>
+    <?php include 'layout/footer.php' ?>
--- layout/navbar.php
+++ layout/navbar.php
@@ -1,7 +1,7 @@
 <div class="fixed w-full text-gray-700 bg-black z-50 shadow-md">
-  <div x-data="{ open: false }" class="flex flex-col max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
+  <div x-data="{ open: false }" class="flex flex-col max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-14">
     <div class="p-3 flex flex-row items-center justify-between">
-    <img class="w-16" src="/public/images/logo.png" alt="BVR Property">
+      <a href="index.php"><img class="h-10" src="/public/images/logo.png" alt="BVR Property"></a>
       <button class="md:hidden rounded-lg focus:outline-none focus:shadow-outline" @click="open = !open">
         <svg fill="currentColor" viewBox="0 0 20 20" class="w-6 h-6">
           <path x-show="!open" fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z" clip-rule="evenodd"></path>
@@ -9,24 +9,90 @@
         </svg>
       </button>
     </div>
-    <nav :class="{'flex': open, 'hidden': !open}" class="flex-col flex-grow pb-4 md:pb-0 hidden md:flex md:justify-end md:flex-row">
-      <a class="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-white focus:text-white hover:bg-red-500 focus:outline-none focus:shadow-outline" href="/tentangkami.php">Tentang Kami</a>
-      <div @click.away="open = false" class="relative" x-data="{ open: false }">
-        <button @click="open = !open" class="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:w-auto md:inline md:mt-0 md:ml-4 hover:text-white focus:text-white hover:bg-red-500 focus:bg-red-500 focus:outline-none focus:shadow-outline">
-          <span>Kerja Kami</span>
-          <svg fill="currentColor" viewBox="0 0 20 20" :class="{'rotate-180': open, 'rotate-0': !open}" class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1">
-            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+    <nav :class="{'flex': open, 'hidden': !open}" class="flex-col flex-grow pb-4 md:pb-0 hidden md:flex md:justify-center md:flex-row ">
+      <a class="text-white px-4 py-2 mt-2 text-sm sm:text-base font-normal bg-transparent rounded dark-mode:bg-transparent md:mt-0 hover:bg-red-600" href="/index.php">Home</a>
+      <!-- Rental -->
+      <div class="dropdown inline-block relative">
+        <button class=" text-white hover:bg-red-600  py-2 px-4 rounded inline-flex items-center">
+          <span class="mr-1">Rental</span>
+          <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
           </svg>
         </button>
-        <div x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48">
-          <div class="px-2 py-2 bg-white rounded-md shadow dark-mode:bg-gray-800">
-            <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-white focus:text-white hover:bg-red-500 focus:bg-red-500 focus:outline-none focus:shadow-outline" href="/kerjakami-tk.php">Taman Kanak - Kanak</a>
-            <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-white focus:text-white hover:bg-red-500 focus:bg-red-500 focus:outline-none focus:shadow-outline" href="/kerjakami-sd.php">Sekolah Dasar</a>
-          </div>
-        </div>
+        <ul class="dropdown-menu absolute hidden w-40 text-white pt-1">
+          <li class=""><a class="rounded-t  bg-gray-900 hover:bg-red-600 py-2 px-4 block whitespace-no-wrap" href="#">Yearly</a></li>
+          <li class=""><a class="rounded-b bg-gray-900  hover:bg-red-600 py-2 px-4 block whitespace-no-wrap" href="#">Monthly</a></li>
+        </ul>
       </div>
-      <a class="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-white focus:text-white hover:bg-red-500 focus:bg-red-500 focus:outline-none focus:shadow-outline" href="#">Publikasi</a>
-      <a class="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-white focus:text-white hover:bg-red-500 focus:bg-red-500 focus:outline-none focus:shadow-outline" href="#">Informasi</a>    
+      <!-- Villa -->
+      <div class="dropdown inline-block relative">
+        <button class=" text-white hover:bg-red-600 py-2 px-4 rounded inline-flex items-center">
+          <span class="mr-1">Villa</span>
+          <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+          </svg>
+        </button>
+        <ul class="dropdown-menu absolute hidden w-28 text-white pt-1">
+          <li class=""><a class="rounded-t  bg-gray-900 hover:bg-red-600 py-2 px-4 block whitespace-no-wrap" href="#">Leasehold</a></li>
+          <li class=""><a class="rounded-b bg-gray-900  hover:bg-red-600 py-2 px-4 block whitespace-no-wrap" href="#">Freehold</a></li>
+        </ul>
+      </div>
+      <!-- Land -->
+      <div class="dropdown  md:inline-block relative">
+        <button class=" text-white hover:bg-red-600 py-2 px-4 rounded inline-flex items-center">
+          <span class="mr-1">Land</span>
+          <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+          </svg>
+        </button>
+        <ul class="dropdown-menu absolute hidden w-28 text-white pt-1">
+          <li class=""><a class="rounded-t  bg-gray-900 hover:bg-red-600 py-2 px-4 block whitespace-no-wrap" href="#">Leasehold</a></li>
+          <li class=""><a class="rounded-b bg-gray-900  hover:bg-red-600 py-2 px-4 block whitespace-no-wrap" href="#">Freehold</a></li>
+        </ul>
+      </div>
+      <!-- About Us -->
+      <div class="dropdown inline-block relative">
+        <button class=" text-white hover:bg-red-600 py-2 px-4 rounded inline-flex items-center">
+          <span class="mr-1">About Us</span>
+          <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+          </svg>
+        </button>
+        <ul class="dropdown-menu absolute hidden w-56 text-white pt-1">
+          <li class=""><a class="rounded-t  bg-gray-900 hover:bg-red-600 py-2 px-4 block" href="#">Meet the expert</a></li>
+          <li class=""><a class="bg-gray-900 hover:bg-red-600 py-2 px-4 block" href="#">FAQs Pages</a></li>
+          <li class=""><a class="bg-gray-900 hover:bg-red-600 py-2 px-4 block" href="#">Testimonials</a></li>
+          <li class=""><a class="bg-gray-900 hover:bg-red-600 py-2 px-4 block" href="#">Francising, Licensing & Partership</a></li>
+          <li class=""><a class="rounded-b bg-gray-900  hover:bg-red-600 py-2 px-4 block" href="#">Contact Us</a></li>
+        </ul>
+      </div>
+      <a class="text-white px-4 py-2 mt-2 text-sm sm:text-base font-normal bg-transparent rounded dark-mode:bg-transparent md:mt-0 hover:bg-red-600" href="/index.php">Blog</a>
+      <!-- Land -->
+      <div class="md:hidden dropdown inline-block relative md:justify-end">
+        <button class=" text-white hover:bg-red-600 py-2 px-4 rounded inline-flex items-center">
+          <span class="mr-1">IDR</span>
+          <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+          </svg>
+        </button>
+        <ul class="dropdown-menu absolute hidden w-28 text-white pt-1">
+          <li class=""><a class="rounded bg-gray-900 hover:bg-red-600 py-2 px-4 block whitespace-no-wrap" href="#">USD</a></li>
+        </ul>
+      </div>
     </nav>
+    <!-- Land -->
+    <div class="hidden dropdown md:inline-block relative md:justify-end">
+      <button class=" text-white hover:bg-red-600 py-2 px-4 rounded inline-flex items-center">
+        <span class="mr-1">IDR</span>
+        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+          <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+        </svg>
+      </button>
+      <ul class="dropdown-menu absolute hidden w-28 text-white pt-1">
+        <li class=""><a class="rounded bg-gray-900 hover:bg-red-600 py-2 px-4 block whitespace-no-wrap" id="">USD</a></li>
+      </ul>
+    </div>
   </div>
+</div>
+<div class="h-16 w-full text-gray-700 bg-black z-50 shadow-md">
 </div>
