7db773073f479568239979b9825074c52733dee9
yogamahhendra <yoga.mahendra2000@gmail.com>
2021-12-08 17:25:09 +0800

    responsif home

--- index2.php
+++ index2.php
@@ -38,7 +38,7 @@
 
 <body class="bg-gray-200">
     <?php include 'layout/navbar.php' ?>
-    <div class="mx-auto w-9/12 h-96">
+    <div class="mx-auto w-10/12 lg:w-9/12 h-96">
         <svg height="3" class="w-full my-4">
             <line x1="0" y1="0" x2="30000" y2="0" style="stroke:black;stroke-width:3" />
         </svg>
@@ -71,7 +71,7 @@
         </div>
     </div>
     <!-- Property List -->
-    <div class="mx-auto w-9/12 mt-7">
+    <div class="mx-auto w-10/12 lg:w-9/12 mt-7">
         <div class="flex my-3">
             <p class="flex-none pr-6 font-bold">PROPERTY LIST</p>
             <!-- Line -->
@@ -81,64 +81,6 @@
         </div>
         <!-- Search -->
         <!-- Form 1 -->
-        <!-- desktop -->
-        <div class="hidden lg:block">
-            <div class="flex">
-                <!-- Form Search -->
-                <input class="border-2 border-gray-300 bg-white h-10 px-4 text-sm focus:outline-none w-full lg:w-5/12" type="search" name="search" placeholder="Search">
-                <!-- Dropdown Region -->
-                <select class="ml-5 px-4 border-gray-300 border-2">
-                    <option>Region</option>
-                    <option>Sample</option>
-                    <option>Sample</option>
-                </select>
-                <!-- Dropdown Property -->
-                <select class="ml-5 px-4 border-gray-300 border-2">
-                    <option>Property Type</option>
-                    <option>Sample</option>
-                    <option>Sample</option>
-                </select>
-                <!-- Dropdown MinBedroom -->
-                <select class="ml-5 px-4 border-gray-300 border-2">
-                    <option>Min Bedroom</option>
-                    <option>Sample</option>
-                    <option>Sample</option>
-                </select>
-                <!-- Dropdown Max Bed -->
-                <select class="px-4 border-gray-300 border-2">
-                    <option>Max Bedroom</option>
-                    <option>Sample</option>
-                    <option>Sample</option>
-                </select>
-            </div>
-            <!-- Form 2 -->
-            <div class="flex mt-3 h-10">
-                <!-- Dropdown MinPrice -->
-                <select class="px-4 border-gray-300 border-2">
-                    <option>Min Price</option>
-                    <option>Sample</option>
-                    <option>Sample</option>
-                </select>
-                <!-- Dropdown MaxPrice -->
-                <select class="px-4 border-gray-300 border-2">
-                    <option>Max Price</option>
-                    <option>Sample</option>
-                    <option>Sample</option>
-                </select>
-                <!-- Dropdown Are -->
-                <select class="ml-5 px-4 border-gray-300 border-2">
-                    <option>Are</option>
-                    <option>Sample</option>
-                    <option>Sample</option>
-                </select>
-                <input type='text' placeholder="Min. Area" class="border-2 border-gray-300 bg-white h-10 px-4 text-sm  w-2/12">
-                <input type='text' placeholder="Max. Area" class="border-2 border-gray-300 bg-white h-10 px-4 text-sm  w-2/12">
-                <button class="ml-3 border-2  bg-bvr2 hover:bg-bvr3 text-white h-10 text-center text-base w-1/12">
-                    Search
-                </button>
-            </div>
-        </div>
-        <!-- mobile -->
         <div class="lg:hidden">
             <!-- Form Search -->
             <input class="border-2 border-gray-300 bg-white h-10 px-4 text-sm focus:outline-none w-full lg:w-5/12" type="search" name="search" placeholder="Search">
--- layout/footer.php
+++ layout/footer.php
@@ -1,26 +1,45 @@
 <div class="bg-gray-800 text-gray-300">
-    <div class="mx-44">
+    <div class="w-10/12 lg:w-9/12 mx-auto">
         <div class="py-4 pt-10 w-full flex">
-            <img class="w-52 h-full" src="/public/images/footer-logo.png">
+            <img class="w-36 md:w-44 lg:w-52 h-full" src="/public/images/footer-logo.png">
             <div class="w-full flex justify-end">
-                <a class="px-1.5" href="#">
-                    <i class="fab fa-facebook-square fa-lg"></i>
-                </a>
-                <a class="px-1.5" href="#">
-                    <i class="fab fa-twitter-square fa-lg"></i>
-                </a>
-                <a class="px-1.5" href="#">
-                    <i class="fab fa-linkedin fa-lg"></i>
-                </a>
-                <a class="px-1.5" href="#">
-                    <i class="fab fa-instagram fa-lg"></i>
-                </a>
-                <a class="px-1.5" href="#">
-                    <i class="fab fa-youtube-square fa-lg"></i>
-                </a>
+                <div class="block md:hidden">
+                    <a class="px-0.5 md:px-1 lg:px-1.5" href="#">
+                        <i class="fab fa-facebook-square fa-sm"></i>
+                    </a>
+                    <a class="px-0.5 md:px-1 lg:px-1.5" href="#">
+                        <i class="fab fa-twitter-square fa-sm"></i>
+                    </a>
+                    <a class="px-0.5 md:px-1 lg:px-1.5" href="#">
+                        <i class="fab fa-linkedin fa-sm"></i>
+                    </a>
+                    <a class="px-0.5 md:px-1 lg:px-1.5" href="#">
+                        <i class="fab fa-instagram fa-sm"></i>
+                    </a>
+                    <a class="px-0.5 md:px-1 lg:px-1.5" href="#">
+                        <i class="fab fa-youtube-square fa-sm"></i>
+                    </a>
+                </div>
+                <div class="hidden md:block">
+                    <a class="px-0.5 md:px-1 lg:px-1.5" href="#">
+                        <i class="fab fa-facebook-square fa-lg"></i>
+                    </a>
+                    <a class="px-0.5 md:px-1 lg:px-1.5" href="#">
+                        <i class="fab fa-twitter-square fa-lg"></i>
+                    </a>
+                    <a class="px-0.5 md:px-1 lg:px-1.5" href="#">
+                        <i class="fab fa-linkedin fa-lg"></i>
+                    </a>
+                    <a class="px-0.5 md:px-1 lg:px-1.5" href="#">
+                        <i class="fab fa-instagram fa-lg"></i>
+                    </a>
+                    <a class="px-0.5 md:px-1 lg: px-1.5" href="#">
+                        <i class="fab fa-youtube-square fa-lg"></i>
+                    </a>
+                </div>
             </div>
         </div>
-        <div class="grid grid-cols-5 gap-4">
+        <div class="grid grid-cols-2 md:grid-cols-5 gap-4">
             <div class="col-span-1 text-sm">
                 <p class="my-2">Home</p>
                 <p class="my-2">Blog</p>
@@ -45,20 +64,20 @@
                     <p class="my-2">info@bvrproperty.com</p>
                 </div>
             </div>
-            <div class="col-span-1 text-sm">
+            <div class="col-span-2 md:col-span-1 text-sm">
                 <p class="my-2">Name</p>
                 <input type='text' placeholder="" class="border-2 border-gray-300 text-black bg-white h-8 px-1 text-sm  w-full">
                 <p class="my-2">Email</p>
                 <input type='text' placeholder="" class="border-2 border-gray-300 text-black bg-white h-8 px-1 text-sm  w-full">
-                <button class="mt-4 border-2 border-bvr3 bg-bvr2 hover:bg-bvr3 text-white h-10 text-center text-base w-5/12">
+                <button class="mt-4 border-2 border-bvr3 bg-bvr2 hover:bg-bvr3 px-4 text-white h-10 text-center text-base w-max">
                     Subscribe
                 </button>
             </div>
         </div>
         <!-- Copyright -->
-        <div class="py-4 pt-10 w-full flex">
-            <p class="w-full">© 2020. All right reserved</p>
-            <div class="w-full flex justify-end">
+        <div class="py-4 pt-10 w-full lg:flex">
+            <p class="w-full text-center lg:text-left">© 2020. All right reserved</p>
+            <div class="w-full lg:flex text-center lg:justify-end">
                 Powered by BVR Digital
             </div>
         </div>
